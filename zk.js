db8cef81580fea88c182c75fe963fbcfffe828d0
sodabox <sodabox.io@gmail.com>
2012-08-22 15:26:18 +0900

    minor fix.

--- zk.js
+++ zk.js
@@ -20,25 +20,22 @@ module.exports = function(options, cbFn) {
 		host_order_deterministic: false
 	});
 
-
-	console.log(options);
-
 	zk.connect(function (err) {
+		
 		if(err) throw err;
 		
-		console.log ("zk session established, id=%s", zk.client_id);
+		console.log (" [CONNECT] id=%s", zk.client_id);
 
 		zk.a_exists(rootPath, null, function ( rc, error, stat ){
 
-			console.log(rc+", "+error+", "+stat );
+			console.log("  [EXISTES] "+rc+", "+error+", "+stat );
 
-			if(rc != 0){ // 존재하지 않는다면, 
+			if(rc != 0){ // 존재하지 않는다면, rootPath 생성 
 				zk.a_create (rootPath, null, ZooKeeper.ZOO_PERSISTENT, function (rc, error, path)  {
 					if (rc != 0) {
-						console.log ("zk node create result: %d, error: '%s', path=%s", rc, error, path);
-						// ERROR !!!!
+						// ERROR :: 존재하지 않아서 생성했는데 에러 난 경우
+						console.error ("  **ERROR** ("+rootPath+") %d, error: '%s', path=%s", rc, error, path);
 					} else {
-						console.log ("created zk node %s", path);
 						createServerZNode();
 					}
 				});
@@ -52,47 +49,44 @@ module.exports = function(options, cbFn) {
 	var createServerZNode = function(){
 
 		console.log('PATH - '+rootPath+"/"+options.host+":"+options.port);
-		zk.aw_exists(rootPath, 
-			function ( type, state, path ){
-				console.log('WATCH '+type+','+state+','+path);
-
-			}, 
+		zk.aw_exists(rootPath, null,
 			function ( rc, error, stat ){
 				console.log("---- "+rc+", "+error+", "+stat );
 				zk.a_create (rootPath+"/"+options.host+":"+options.port, options.channel, ZooKeeper.ZOO_EPHEMERAL, function (rc, error, path)  {
 					if (rc != 0) {
-						console.log ("---- zk node create result: %d, error: '%s', path=%s", rc, error, path);
+						// ERROR :: 존재하지 않아서 생성했는데 에러 난 경우
+						console.error ("  **ERROR** ("+rootPath+"/"+options.host+":"+options.port+") %d, error: '%s', path=%s", rc, error, path);
 						// ERROR
 					} else {
-						console.log ("---- created zk node %s", path);
+						console.log ("  [CREATE] %s", path);
 						getSocketServers();
-						
 					}
-
-					
-
-
 			});
 		});
 	};
 
-
 	var getSocketServers = function(){
-		zk.a_get_children(rootPath,null,
+		zk.aw_get_children(
+			rootPath,
+			function ( type, state, path ){
+				console.log('  [WATCH] '+type+','+state+','+path);
+				getSocketServers();
+			},
 			function(rc,error,children){
-			var result=[];
-			var ttt = '';
-			console.log(rc+','+error+','+children);
-			if(rc==0){
-
-				children.forEach(function(child){
-					ttt = ttt + ' / '+child;
-					console.log(' >>> '+child);
-				});
-				cbFn(ttt);
+				var result=[];
+				var ttt = '';
+				console.log(rc+','+error+','+children);
+				if(rc==0){
+					cbFn(children);
+					/*
+					children.forEach(function(child){
+						ttt = ttt + ' / '+child;
+						console.log(' >>> '+child);
+					});
+					*/
+				}
 			}
-
-		});
+		);
 
 	};
 
