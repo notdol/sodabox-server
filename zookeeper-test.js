561d17a8dc471a7934a697bfbcd4d2c3a5ff5b25
sodabox <sodabox.io@gmail.com>
2012-08-22 15:37:22 +0900

    Update zookeeper-test.js

--- zookeeper-test.js
+++ zookeeper-test.js
@@ -1,13 +1,21 @@
 var ZK = require('./zk.js');
 
+var socketServerList = [];
 
-var zkClient = ZK({
-	connect: 'localhost:2181',
-	host: '123.456.789',
-	port: '8080',
-	channel: 'CH001'
-},
-function(data){
-	console.log('   000 '+data);
-}
+
+var zkClient = ZK(
+	{
+		connect: 	'localhost:2181',
+		host: 		'123.456.789',
+		port: 		'8080',
+		channel: 	'CH001'
+	},
+	function(datas){
+		socketServerList.length = 0;
+		console.log('callback Socket ServerList : '+datas);
+		datas.forEach(function(data){
+			console.log(' >>> '+data);
+			socketServerList.push(data);
+		});
+	}
 );
